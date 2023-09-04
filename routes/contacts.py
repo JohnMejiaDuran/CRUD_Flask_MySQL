9609608e4a1f4205b3dd83f5c24765885368c110
John <johnmejiaduran@gmail.com>
2023-09-04 18:03:10 -0500

    add templates

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,11 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 contacts = Blueprint('contacts',__name__)
 
 @contacts.route("/")
 def index():
-    return "Contacts List"
+    return render_template('index.html')
 
 @contacts.route("/new")
 def add_contact():
     return "guardando un contacto"
+
+@contacts.route("/about")
+def about():
+    return render_template('about.html')
+
+@contacts.route("/update")
+def update_contact():
+    return "update the contact"
